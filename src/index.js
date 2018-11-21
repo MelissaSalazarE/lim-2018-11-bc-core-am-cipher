9f41e498965e72307c9eae18fb6751300ab007f3
melilove <melissa.salazar13@yahoo.com>
2018-11-21 13:14:33 -0500

    cambios en js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function cifrar() {
+/*function cifrar() {
   //alert("hola")
   var palabra = document.getElementById("text-cifrar").value;
   var palabra_codificada = btoa(palabra);
@@ -14,4 +14,39 @@ const ingreso = document.getElementById("btn-encode-cifrar");
 ingreso.addEventListener("click", cifrar);
 
 const ingresode = document.getElementById("btn-encode-decifrar");
-ingresode.addEventListener("click", decifrar);
+ingresode.addEventListener("click", decifrar);*/
+const btnCipher=document.getElementById('btn-encode-cifrar')
+const message=document.getElementById('text-cifrar');
+const codDes=document.getElementById('input-offset-cifrar');
+let message2=document.getElementById('text-decifrar');
+
+
+function cifrar(word,descod)
+{
+
+let newMessage="";
+
+  for (let i = 0; i < word.length; i++) {
+    let numberAscii =((word[i].charCodeAt())-65+descod)%26+65;
+     //alert(numberAscii);
+    let lettercipher =String.fromCharCode(numberAscii);
+    // alert(lettercipher);
+    newMessage = newMessage+lettercipher;
+  }
+  //return newMessage;
+  //alert(newMessage);
+  console.log(newMessage);
+}
+
+function hacerClick() {
+  //console.log(typeof message.value);
+  //console.log(typeof parseInt(codDes.value));
+  let mensaje = message.value;
+  let numero = parseInt(codDes.value);
+  //cifrar(mensaje,numero);
+  //message2.value=mensaje;
+  message2 = cifrar(mensaje, numero);
+
+}
+
+btnCipher.addEventListener("click",hacerClick)
