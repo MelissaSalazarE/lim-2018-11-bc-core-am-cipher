3a42b70b1e76ec32e96644cc3d5306617b37446f
melilove <melissa.salazar13@yahoo.com>
2018-11-22 12:35:38 -0500

    ya las funciones estan en cipher y mas ordenada

--- src/cipher.js
+++ src/cipher.js
@@ -6,19 +6,15 @@ window.cipher = {
       for (let i = 0; i < string.length; i++) {
         if (string[i].charCodeAt() >= 65 && string[i].charCodeAt() <=90) {
           let numberAscii =((string[i].charCodeAt())-65+offset)%26+65;
-           //alert(numberAscii);
           let lettercipher =String.fromCharCode(numberAscii);
-          // alert(lettercipher);
           newMessage = newMessage+lettercipher;
         } else {
           newMessage = newMessage+string[i];
         }
-
       }
       return newMessage;
-      //alert(newMessage);
-      //console.log(newMessage);
     },
+
   decode: (offset,string) => {
 
     let newMessage="";
@@ -26,14 +22,11 @@ window.cipher = {
       for (let i = 0; i < string.length; i++) {
     if (string[i].charCodeAt() >= 65 && string[i].charCodeAt() <=90) {
         let numberAscii =((string[i].charCodeAt())+65-offset)%26+65;
-         //alert(numberAscii);
         let lettercipher =String.fromCharCode(numberAscii);
-        // alert(lettercipher);
         newMessage = newMessage+lettercipher;
       } else {
         newMessage = newMessage+string[i];
       }
-
     }
       return newMessage;
 }
--- src/index.js
+++ src/index.js
@@ -1,43 +1,20 @@
-// este es un modelo de cifrado mas basico que me sirvio para evaluar mis botones//
-/*function cifrar() {
-  //alert("hola")
-  var palabra = document.getElementById("text-cifrar").value;
-  var palabra_codificada = btoa(palabra);
-  document.getElementById("text-cifrar").value = palabra_codificada;
-}
-function decifrar() {
-  var palabra = document.getElementById("text-decifrar").value;
-  var palabra_codificada = atob(palabra);
-  document.getElementById("text-decifrar").value = palabra_codificada;
-}
+//Ahora hallaremos el modelo que cifra
 
-const ingreso = document.getElementById("btn-encode-cifrar");
-ingreso.addEventListener("click", cifrar);
-
-const ingresode = document.getElementById("btn-encode-decifrar");
-ingresode.addEventListener("click", decifrar);*/
-// Hallamos la funcion de decifrar utilizando el metodo Ascii para ello aplicamos un for y llamamos cuatro constatntes//
 const btnCipher=document.getElementById('btn-encode-cifrar')
 const message=document.getElementById('text-cifrar');
 const codDes=document.getElementById('input-offset-cifrar');
 const message2=document.getElementById('text-cifrar');
 
 function hacerClick() {
-  //console.log(typeof message.value);
-  //console.log(typeof parseInt(codDes.value));
   let mensaje = message.value;
   let numero = parseInt(codDes.value);
-  //cifrar(mensaje,numero);
-  //message2.value=mensaje;
-let newMessage=cipher.encode(numero,mensaje);
-  //message2.value = cipher.encode(numero,mensaje);
-  message2.value = newMessage;
-  //console.log(newessage1);
+  let newMessage= cipher.encode(numero,mensaje);
+return message2.value = newMessage;
 }
 
 btnCipher.addEventListener("click",hacerClick)
 
-//Ahora hallaremos el modelo que decifra//
+//Ahora hallaremos el modelo que decifra
 
 const btnCipher1=document.getElementById('btn-encode-decifrar')
 const message3=document.getElementById('text-decifrar');
@@ -45,14 +22,10 @@ const codDes1=document.getElementById('input-offset-decifrar');
 const message4=document.getElementById('text-decifrar');
 
 function hacerClick2() {
-  //console.log(typeof message.value);
-  //console.log(typeof parseInt(codDes.value));
   let mensaje = message3.value;
   let numero = parseInt(codDes1.value);
-  //cifrar(mensaje,numero);
-  //message2.value=mensaje
-let newMessage=cipher.decode(numero,mensaje);
-  message4.value = newMessage;
+  let newMessage = cipher.decode(numero,mensaje);
+return message4.value = newMessage;
 }
 
 btnCipher1.addEventListener("click",hacerClick2)
